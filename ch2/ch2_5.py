57fe1b6eacf2756d919fbe3c6107341f01ec651d
lecielpourlamer <88413718+lecielpourlamer@users.noreply.github.com>
2025-04-08 09:26:53 +0900

    first commit

---
+++ ch2/ch2_5.py
@@ -0,0 +1,9 @@
+name = "연탄이"
+animal = "개"
+age = 4
+hobby = "산책"
+is_male = True
+
+print("반려동물을 소개해 주세요")
+print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "예요.")
+print(name + "는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
