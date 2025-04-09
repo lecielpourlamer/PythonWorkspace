9c580504226f679d16fa0e346592fb6c1069039a
lecielpourlamer <88413718+lecielpourlamer@users.noreply.github.com>
2025-04-09 09:03:40 +0900

    3장 연산자 미완료

---
+++ ch3/ch3_2.py
@@ -0,0 +1,10 @@
+number = 2 + 3 * 4
+print(number)
+print()
+
+# number = 2 + 3 * 4 + 2
+# print(number)
+
+print()
+number = number + 2
+print(number)
---
+++ ch3/ch3_3.py
@@ -0,0 +1,30 @@
+number = 2 + 3 * 4
+print(number)   # 14
+
+print()
+number += 2     # number = number + 2
+print(number)   # 16
+
+print()
+number -= 2     # number = number - 2
+print(number)   # 14
+
+print()
+number *= 2     # number = number * 2
+print(number)   # 28
+
+print()
+number /= 2     # number = number / 2
+print(number)   # 14.0
+
+print()
+number **= 2    # number = number ** 2
+print(number)   # 196.0
+
+print()
+number //= 2    # number = number // 2
+print(number)   # 98.0
+
+print()
+number %= 2     #number = number % 2
+print(number)   # 0.0
