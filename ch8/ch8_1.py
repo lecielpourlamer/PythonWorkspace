46722548a065fe04b711aad1383e4322eb6bcc26
lecielpourlamer <88413718+lecielpourlamer@users.noreply.github.com>
2025-04-16 08:41:50 +0900

    ch8 ~228p 8.2.2

---
+++ ch8/ch8_1.py
@@ -0,0 +1,7 @@
+answer = input("아무 값이나 입력하세요 : ")
+print("입력한 값은 " + answer + "입니다.")
+print(type(answer))
+print()
+print(type(int(answer)))
+answer = 10
+print(type(answer))
---
+++ ch8/ch8_4.py
@@ -0,0 +1,9 @@
+import sys
+
+print("파이썬", "자바", file=sys.stdout)
+# sys.stdout은 표준 출력(Standard Output) -> VSCode 터미널에 결과 출력
+# 파이썬 자바
+
+print("파이썬", "자바", file=sys.stderr)
+# sys.stderr은 표준 오류(Standard Error) -> 오류 발생 시 터미털에 오류 메시지 출력
+# 파이썬 자바
